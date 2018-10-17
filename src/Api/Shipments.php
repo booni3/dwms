e2e79281e10210a1429e7718d6941a879fecdfd0
booni3 <adam@profilestudio.com>
2018-10-17 22:26:53 +0100

    add page

--- src/Api/Shipments.php
+++ src/Api/Shipments.php
@@ -10,9 +10,10 @@ class Shipments extends Api
 
     }
 
-    public function getShipments($updated_from = null, $created_from = null, $updated_to = null, $created_to = null)
+    public function getShipments($page = 1, $updated_from = null, $created_from = null, $updated_to = null, $created_to = null)
     {
         return $this->_get('get-shipments',[
+            'page' => $page,
             'updated_from' => $updated_from,
             'created_from' => $created_from,
             'updated_to' => $updated_to,
